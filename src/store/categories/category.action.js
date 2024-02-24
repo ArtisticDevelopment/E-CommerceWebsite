3e7ee55988c62a888257ba65d9c8b60c69c299a8
Silas Green <scgreen160@gmail.com>
2024-02-24 10:04:35 -0700

    small progress

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,9 +1,31 @@
 import createAction from "../../utils/reducer/reducer.utils";
-
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 import { CATEGORIES_ACTION_TYPES } from "./category.types";
 
 //after an action is set it runs through ALL the reducers to
 //find it's match and execute that action
-export const setCategories = (categoriesArray) => {
-  return createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
+
+export const fetchCategoriesStart = () => {
+  return createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+};
+export const fetchCategoriesSuccess = (categoriesArray) => {
+  return createAction(
+    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+    categoriesArray
+  );
+};
+export const fetchCategoriesFailed = () => {
+  return createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+};
+
+export const fetchCategoriesAsync = () => {
+  return async (dispatch) => {
+    dispatch(fetchCategoriesStart());
+    try {
+      const categoriesArray = await getCategoriesAndDocuments("categories");
+      fetchCategoriesSuccess(categoriesArray);
+    } catch (error) {
+      dispatch(fetchCategoriesFailed(error));
+    }
+  };
 };
--- src/store/store.js
+++ src/store/store.js
@@ -1,24 +1,29 @@
 import { compose, createStore, applyMiddleware } from "redux";
-
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
+import { thunk } from "redux-thunk";
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-  console.log("type:", action.type);
-  console.log("payload:", action.payload);
-
-  console.log("current state:", store.getState());
-
-  next(action);
-
-  console.log("next state:", store.getState());
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  blacklist: ["user"],
 };
 
-const middleWares = [logger];
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middleWares = [
+  process.env.NODE_ENV !== "production" && logger,
+  thunk,
+].filter(Boolean);
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
